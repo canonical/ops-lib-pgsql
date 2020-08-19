7a742c371c9f74569985a8da72f0346042e1b29d
Stuart Bishop <stuart.bishop@canonical.com>
2020-08-19 15:59:53 +1000

    Python packaging support

---
+++ setup.py
@@ -0,0 +1,39 @@
+# This file is part of the interface-pgsql component for Juju Operator
+# Framework Charms.
+# Copyright 2020 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the Lesser GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="interface-pgsql",
+    version="1.0.0",
+    author="PostgreSQL Charmers",
+    author_email="postgresql-charmers@lists.launchpad.net",
+    description="PostgreSQL database relation for Juju Operator Framework Charms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/canonical/interface-pgsql",
+    packages=['pgsql', 'opslib.pgsql'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
